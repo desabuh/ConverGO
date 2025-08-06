b5ed2f29c850b35912c5c0f315d609660ed6a953
Sabuh <mastersabuh@gmail.com>
2025-08-06 15:57:22 +0200

    added basic Json(De)Encoder

--- p2p/message.go
+++ p2p/message.go
@@ -1,5 +1,11 @@
 package p2p
 
+import (
+	"encoding/json"
+
+	"github.com/google/uuid"
+)
+
 type Message struct {
 	PayLoad   string   `json:"Payload"`
 	Protocoll string   `json:"Prot"`
@@ -7,6 +13,28 @@ type Message struct {
 }
 
 type UserData struct {
-	Id       string `json:"Id"`
-	Username string `json:"Name"`
+	Id       uuid.UUID `json:"Id"`
+	Username string    `json:"Name"`
+}
+
+type Encoder interface {
+	Encode(msg Message) ([]byte, error)
+}
+
+type Decoder interface {
+	Decode(data []byte) (Message, error)
+}
+
+type JsonEncoder struct{}
+
+func (e *JsonEncoder) Encode(msg Message) ([]byte, error) {
+	return json.Marshal(msg)
+}
+
+type JsonDecoder struct{}
+
+func (d *JsonDecoder) Decode(data []byte) (Message, error) {
+	var msg Message
+	err := json.Unmarshal(data, &msg)
+	return msg, err
 }
