7d47806ede2ab5480243d1ad774b5f521e4cf096
Sabuh <mastersabuh@gmail.com>
2025-09-10 11:48:15 +0200

    develop a basic lamport clock syst

---
+++ cvrdt/clock.go
@@ -0,0 +1,27 @@
+package cvrdt
+
+const MaxValue = 1<<31 - 1
+
+type LamportClock struct {
+	counter int
+}
+
+func (l *LamportClock) Increment() int {
+	if l.counter < MaxValue {
+		l.counter += 1
+	}
+	return l.counter
+}
+
+func (l *LamportClock) Value() int {
+	return l.counter
+}
+
+func (l *LamportClock) Update(received int) {
+	if l.counter < MaxValue {
+		l.counter = max(received, l.counter) + 1
+		if l.counter > MaxValue {
+			l.counter = MaxValue
+		}
+	}
+}
