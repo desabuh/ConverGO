31b5ce62bff6b222220454606d1568afba13a345
Sabuh <mastersabuh@gmail.com>
2025-12-02 00:23:30 +0100

    added local and global operation types, general CRDTOperation interface

--- cvrdt/operations.go
+++ cvrdt/operations.go
@@ -1,5 +1,7 @@
 package cvrdt
 
+import "fmt"
+
 type OpType int
 
 const (
@@ -7,7 +9,27 @@ const (
 	Deletion
 )
 
+type CRDTOperation interface {
+	ID() string
+}
+
 type WootOperation struct {
 	character WCharacter
 	opType    OpType
+}
+
+func (w WootOperation) ID() string {
+	return w.character.id.siteId + "_" + fmt.Sprint(w.character.id.clockValue)
+}
+
+type LocalOperation struct {
+	opType  OpType
+	pos     int
+	content string
+
+	id string
+}
+
+func (o LocalOperation) ID() string {
+	return o.id
 }
