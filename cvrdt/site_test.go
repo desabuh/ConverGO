ad6b46c1bfcef2dbe903be27731b56b3b66fb695
Sabuh <mastersabuh@gmail.com>
2025-12-02 00:28:10 +0100

    refactoring the testing suite work along general improved crdt api

--- cvrdt/site_test.go
+++ cvrdt/site_test.go
@@ -1,151 +1,189 @@
 package cvrdt
 
 import (
-	"fmt"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateNewSite(t *testing.T) {
-	const NUM_DEFAULT_CHARS = 2
-	const DEFAULT_CLOCK_VALUE = 0
-	const SITE_ID = "1"
-	const EMPTY_PENDING_OP = 0
-
-	site := NewSite(SITE_ID)
-
-	assert.True(t, site.siteId == SITE_ID, "The site id should be "+SITE_ID)
-
-	assert.True(t, site.characters.GetLen() == NUM_DEFAULT_CHARS, "The site should have by default a start and end WChar")
-
-	startWChar := site.characters.sequence[0]
-	endWChar := site.characters.sequence[1]
-
-	assert.True(t, startWChar.alphaValue == SPECIAL_START_CHAR && endWChar.alphaValue == SPECIAL_END_CHAR, "Start and End Wchar use special Char "+SPECIAL_START_CHAR+" and "+SPECIAL_END_CHAR)
-
-	startWcharId := startWChar.id
-	endWcharId := endWChar.id
-
-	assert.False(t, startWcharId.Equals(endWcharId), "Default start and end char should be different")
-
-	assert.True(t, startWcharId.siteId == endWcharId.siteId, "Though charId are different their site id portion should be the same")
-
-	assert.True(t, site.clock.Value() == DEFAULT_CLOCK_VALUE, "At the moment of creation clock value should be "+fmt.Sprint(DEFAULT_CLOCK_VALUE))
-
-	assert.True(t, len(site.pendingOpsCh) == EMPTY_PENDING_OP, "At the moment of creation there should be no pending operations")
-
-}
-
-func TestLocalInsertDelete(t *testing.T) {
-	const SITE_ID = "1"
-	const ELEMENT_INSERT = "A"
-	const INSERT_OP = Insertion
-	const DELETE_OP = Deletion
-	const INSERT_AT_START = 0
-	const DELETE_AT_FIRST = 0
-
-	const ORIGINAL_STATE = SPECIAL_START_CHAR + SPECIAL_END_CHAR
-	const INSERT_STATE = SPECIAL_START_CHAR + ELEMENT_INSERT + SPECIAL_END_CHAR
-
-	site := NewSite(SITE_ID)
-
-	assert.Equal(t, string(site.GetCurrentData()), ORIGINAL_STATE)
-
-	op, err := site.GenerateIns(INSERT_AT_START, ELEMENT_INSERT)
-
-	assert.Nil(t, err, "Insertion at start should be successfull")
-
-	assert.True(t, op.character.alphaValue == ELEMENT_INSERT, "Operation returned from insertion should provide the Wchar generated")
-
-	assert.True(t, op.opType == INSERT_OP, "Result should be "+fmt.Sprint(INSERT_OP))
-
-	assert.Equal(t, string(site.GetCurrentData()), INSERT_STATE)
-
-	op, err = site.GenerateDel(DELETE_AT_FIRST)
-
-	assert.Nil(t, err, "Deletion at start should be successfull")
-
-	assert.True(t, op.character.alphaValue == ELEMENT_INSERT, "Operation returned from deletion should provide removed character "+ELEMENT_INSERT)
-
-	assert.True(t, op.opType == DELETE_OP, "Result should be "+fmt.Sprint(DELETE_OP))
-
-	assert.Equal(t, string(site.GetCurrentData()), ORIGINAL_STATE)
-
-	op, err = site.GenerateDel(DELETE_AT_FIRST)
-
-	assert.NotNil(t, err, "Deletion should not be possible if no other Wchar are present")
-
-	assert.Equal(t, string(site.GetCurrentData()), ORIGINAL_STATE)
-
-}
-
-func TestConcurrentInsert(t *testing.T) {
+func TestConcurrentInsertCRDT(t *testing.T) {
 	const SITE_ID_1 = "1"
 	const SITE_ID_2 = "2"
-	const ELEMENT_INSERT = "A"
-	const INITIAL_EXPECTED_STATE = SPECIAL_START_CHAR + SPECIAL_END_CHAR
-	const FINAL_EXPECTED_STATE = SPECIAL_START_CHAR + ELEMENT_INSERT + SPECIAL_END_CHAR
 
-	site1 := NewSite(SITE_ID_1)
-	site2 := NewSite(SITE_ID_2)
+	const INSERT_AT_START = 0
 
-	go site1.ReceptionLoop(100 * time.Millisecond)
-	go site2.ReceptionLoop(100 * time.Millisecond)
+	const INSERT_1 = "Hi "
+	const INSERT_2 = "User"
 
-	assert.Equal(t, string(site1.GetCurrentData()), INITIAL_EXPECTED_STATE, "Site1's initial state should match the expected state")
+	var data Cvrdt[CvRDTState, []byte] = NewWootCvrdt(SITE_ID_1)
 
-	op, err := site1.GenerateIns(0, ELEMENT_INSERT)
+	var localOp LocalOperation = LocalOperation{
+		opType:  Insertion,
+		pos:     INSERT_AT_START,
+		content: INSERT_1,
+		id:      SITE_ID_1,
+	}
 
-	assert.Nil(t, err, "Local Insertion at site1 should be successful")
-	assert.True(t, op.opType == Insertion, "Operation should be of type Insertion")
+	err := data.UpdateState(GetNewStateFromOp(localOp))
 
-	assert.Equal(t, string(site1.GetCurrentData()), FINAL_EXPECTED_STATE, "Site1's state should match the expected state")
+	assert.Nil(t, err, "Update State in "+SITE_ID_1+" should be successfull")
 
-	assert.Equal(t, string(site2.GetCurrentData()), INITIAL_EXPECTED_STATE, "Site2's initial state should match the expected state")
+	assert.Equal(t, INSERT_1, string(data.GetStateRappr()))
 
-	site2.QueueOp(op)
+	var data2 Cvrdt[CvRDTState, []byte] = NewWootCvrdt(SITE_ID_2)
 
-	time.Sleep(1 * time.Second)
+	var localOp2 LocalOperation = LocalOperation{
+		opType:  Insertion,
+		pos:     INSERT_AT_START,
+		content: INSERT_2,
+		id:      SITE_ID_2,
+	}
 
-	assert.Equal(t, string(site2.GetCurrentData()), FINAL_EXPECTED_STATE, "Site2's initial state should match the expected state")
+	err = data2.UpdateState(GetNewStateFromOp(localOp2))
+
+	assert.Nil(t, err, "Update State in "+SITE_ID_2+" should be successfull")
+
+	assert.Equal(t, INSERT_2, string(data2.GetStateRappr()))
+
+	err = data.UpdateState(data2.GetState())
+	assert.Nil(t, err, "Update State from "+SITE_ID_2+" into "+SITE_ID_1+" should be successfull")
+
+	assert.Equal(t, string(data.GetStateRappr()), INSERT_1+INSERT_2)
+
+	err = data2.UpdateState(data.GetState())
+	assert.Nil(t, err, "Update State from "+SITE_ID_1+" into "+SITE_ID_2+" should be successfull")
+
+	assert.Equal(t, data.GetState(), data2.GetState())
+	assert.Equal(t, string(data.GetStateRappr()), string(data2.GetStateRappr()))
 
 }
 
-// func TestConcurrentInsertDelete(t *testing.T) {
-// 	const SITE_ID_1 = "1"
-// 	const SITE_ID_2 = "2"
-// 	const ELEMENT_INSERT_1 = "A"
-// 	const ELEMENT_INSERT_2 = "B"
-// 	//const INSERT_AT_START = 0
-// 	const INSERT_AT_END = 1
+func TestDuplicateInsert(t *testing.T) {
+	const SITE_ID_1 = "1"
+	const INSERT_AT_START = 0
+	const INSERT = "Test"
 
-// 	const EXPECTED_STATE = SPECIAL_START_CHAR + ELEMENT_INSERT_1 + ELEMENT_INSERT_2 + SPECIAL_END_CHAR
+	var data Cvrdt[CvRDTState, []byte] = NewWootCvrdt(SITE_ID_1)
 
-// 	site1 := NewSite(SITE_ID_1)
-// 	site2 := NewSite(SITE_ID_2)
+	var localOpInsert LocalOperation = LocalOperation{
+		opType:  Insertion,
+		pos:     INSERT_AT_START,
+		content: INSERT,
+		id:      SITE_ID_1,
+	}
 
-// 	// Site 1 inserts ELEMENT_INSERT_1 at the start
-// 	//op1, err1 := site1.GenerateIns(INSERT_AT_START, ELEMENT_INSERT_1)
-// 	//assert.Nil(t, err1, "Insertion at site1 should be successful")
+	err := data.UpdateState(GetNewStateFromOp(localOpInsert))
+	assert.Nil(t, err, "Update State in "+SITE_ID_1+" should be successful")
 
-// 	// Site 2 inserts ELEMENT_INSERT_2 at the end
-// 	op2, err2 := site2.GenerateIns(INSERT_AT_END, ELEMENT_INSERT_2)
-// 	assert.Nil(t, err2, "Insertion at site2 should be successful")
+	data.UpdateState(data.GetState())
 
-// 	fmt.Printf("op2: %v\n", op2)
+	assert.Equal(t, string(data.GetStateRappr()), INSERT, "Duplicate Operation should not be repeated")
 
-// 	//go site2.ReceptionLoop(100 * time.Millisecond)
-// 	//site2.QueueOp(op1)
+}
 
-// 	// Apply operations from site2 to site1
-// 	go site1.ReceptionLoop(100 * time.Millisecond)
-// 	site1.QueueOp(op2)
+func TestConcurrentInsertDeleteCRDT(t *testing.T) {
+	const SITE_ID_1 = "1"
+	const SITE_ID_2 = "2"
 
-// 	time.Sleep(1 * time.Second)
+	const INSERT_AT_START = 0
+	const DELETE_AT_START = 0
 
-// 	// Verify that both sites converge to the same state
-// 	assert.Equal(t, string(site1.GetCurrentData()), EXPECTED_STATE, "Site1's state should match the expected state")
-// 	//assert.Equal(t, string(site2.GetCurrentData()), EXPECTED_STATE, "Site2's state should match the expected state")
-// }
+	const INSERT_1 = "Hello"
+	const INITIAL_EXPECTED_STATE = ""
+	const FINAL_EXPECTED_STATE_AFTER_INSERT = INSERT_1
+	const FINAL_EXPECTED_STATE_AFTER_DELETE = INITIAL_EXPECTED_STATE
+
+	var data1 Cvrdt[CvRDTState, []byte] = NewWootCvrdt(SITE_ID_1)
+	var data2 Cvrdt[CvRDTState, []byte] = NewWootCvrdt(SITE_ID_2)
+
+	var localOpInsert LocalOperation = LocalOperation{
+		opType:  Insertion,
+		pos:     INSERT_AT_START,
+		content: INSERT_1,
+		id:      SITE_ID_1,
+	}
+
+	err := data1.UpdateState(GetNewStateFromOp(localOpInsert))
+	assert.Nil(t, err, "Update State in "+SITE_ID_1+" should be successful")
+	assert.Equal(t, string(data1.GetStateRappr()), FINAL_EXPECTED_STATE_AFTER_INSERT)
+
+	err = data2.UpdateState(data1.GetState())
+	assert.Nil(t, err, "Update State from "+SITE_ID_1+" into "+SITE_ID_2+" should be successful")
+	assert.Equal(t, string(data2.GetStateRappr()), FINAL_EXPECTED_STATE_AFTER_INSERT)
+
+	var localOpDelete LocalOperation = LocalOperation{
+		opType: Deletion,
+		pos:    DELETE_AT_START,
+		id:     SITE_ID_1,
+	}
+
+	err = data1.UpdateState(GetNewStateFromOp(localOpDelete))
+	assert.Nil(t, err, "Update State in "+SITE_ID_1+" should be successful")
+	assert.Equal(t, string(data1.GetStateRappr()), FINAL_EXPECTED_STATE_AFTER_DELETE)
+
+	err = data2.UpdateState(data1.GetState())
+	assert.Nil(t, err, "Update State from "+SITE_ID_1+" into "+SITE_ID_2+" should be successful")
+	assert.Equal(t, string(data2.GetStateRappr()), FINAL_EXPECTED_STATE_AFTER_DELETE)
+
+	assert.Equal(t, data1.GetState(), data2.GetState())
+	assert.Equal(t, string(data1.GetStateRappr()), string(data2.GetStateRappr()))
+}
+
+func TestComputeDependandInsertPendings(t *testing.T) {
+	const SITE_ID_1 = "1"
+	const SITE_ID_2 = "2"
+
+	const BEFORE_STR = "First part "
+	const AFTER_STR = "Second part"
+	const EMPTY = ""
+
+	var data1 Cvrdt[CvRDTState, []byte] = NewWootCvrdt(SITE_ID_1)
+
+	var localOpInsert LocalOperation = LocalOperation{
+		opType:  Insertion,
+		pos:     0,
+		content: BEFORE_STR,
+		id:      SITE_ID_1,
+	}
+
+	err := data1.UpdateState(GetNewStateFromOp(localOpInsert))
+	assert.Nil(t, err, "Update State for: "+BEFORE_STR+" in "+SITE_ID_1+" should be successful")
+
+	localOpInsert = LocalOperation{
+		opType:  Insertion,
+		pos:     1,
+		content: AFTER_STR,
+		id:      SITE_ID_1,
+	}
+
+	err = data1.UpdateState(GetNewStateFromOp(localOpInsert))
+	assert.Nil(t, err, "Update State for: "+AFTER_STR+" in "+SITE_ID_1+" should be successful")
+
+	assert.Equal(t, string(data1.GetStateRappr()), BEFORE_STR+AFTER_STR)
+
+	var data2 Cvrdt[CvRDTState, []byte] = NewWootCvrdt(SITE_ID_2)
+
+	var firstSiteState map[CRDTOperation]struct{} = data1.GetState()
+
+	var firstOp, secondOp CRDTOperation
+	i := 0
+	for op := range firstSiteState {
+		if i == 0 {
+			firstOp = op
+		} else if i == 1 {
+			secondOp = op
+		}
+		i++
+	}
+
+	err = data2.UpdateState(GetNewStateFromOp(secondOp))
+	assert.Nil(t, err, "Update State for: "+AFTER_STR+" in "+SITE_ID_2+" should be successful")
+
+	assert.Equal(t, string(data2.GetStateRappr()), EMPTY, "Out of order states should not be immediately added to the CRDT")
+
+	err = data2.UpdateState(GetNewStateFromOp(firstOp))
+	assert.Nil(t, err, "Update State for: "+BEFORE_STR+" in "+SITE_ID_2+" should be successful")
+
+	assert.Equal(t, string(data2.GetStateRappr()), BEFORE_STR+AFTER_STR, "Pendings states should be added after this")
+
+}
