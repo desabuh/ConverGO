24e7fc27ee9cf43cb4e6a063e6abc02e0c03720b
Sabuh <mastersabuh@gmail.com>
2025-12-02 23:12:14 +0100

    update State polling with a factory method for crdt

--- file/polling.go
+++ file/polling.go
@@ -2,25 +2,44 @@ package file
 
 import (
 	"io"
+	"os"
 	"sync"
 	"time"
+
+	"github.com/desabuh/convergo/cvrdt"
 )
 
 // A poller that countinuosly dump every interval some data R from a provider into a writer
 // Note: the safe concurrent access to the provider should be garanteed by the provider itself
 type StatePollingDumper[R any] struct {
-	provider interface{ GetStateRappr() R }
-	writer   io.Writer
-	encode   func(R) ([]byte, error)
-	interval time.Duration
-	stopCh   chan struct{}
-	wg       sync.WaitGroup
+	provider   interface{ GetStateRappr() R }
+	writer     io.Writer
+	encode     func(R) ([]byte, error)
+	onShutDown func() error
+	interval   time.Duration
+	stopCh     chan struct{}
+	wg         sync.WaitGroup
+}
+
+func GetCrdtFilePollingDumper[S cvrdt.Mergeable[S]](crdt cvrdt.Cvrdt[S, []byte], writer *os.File, interval time.Duration) StatePollingDumper[[]byte] {
+
+	return StatePollingDumper[[]byte]{
+		provider: crdt,
+		writer:   writer,
+		encode: func(data []byte) ([]byte, error) { //placeholder will need a change cache
+			return data, nil
+		},
+		onShutDown: func() error { return writer.Close() }, //closure to manage file shutdown
+		interval:   interval,
+		stopCh:     make(chan struct{}),
+	}
 }
 
 func (sd *StatePollingDumper[R]) Run() {
 	defer sd.wg.Done()
 	ticker := time.NewTicker(sd.interval)
 	defer ticker.Stop()
+	defer sd.onShutDown()
 
 	for {
 		select {
@@ -28,6 +47,12 @@ func (sd *StatePollingDumper[R]) Run() {
 			rappr := sd.provider.GetStateRappr()
 
 			b, _ := sd.encode(rappr)
+
+			// Truncate the file before writing to ensure it is overwritten
+			if file, ok := sd.writer.(*os.File); ok {
+				file.Truncate(0)
+				file.Seek(0, 0)
+			}
 
 			sd.writer.Write(b)
 			sd.writer.Write([]byte("\n"))
