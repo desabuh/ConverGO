04c498ad8b9e041e3bc9a3d961d166021c06998a
Sabuh <mastersabuh@gmail.com>
2025-12-04 19:56:13 +0100

    added come useful utils interface  (ObservableState ad ReadOnlyStateStore)

--- utils/interface.go
+++ utils/interface.go
@@ -5,6 +5,12 @@ type Mergeable[T any] interface {
 	Merge(state T) T
 }
 
+// Provide a StateStore that can be also Observed (with a pull approach) and return a rappresentation R of the state S
+type ObservableState[S any, R any] struct {
+	StateStore[S]
+	SnapshotView[R]
+}
+
 // Provide a point-in-time rappresentation of an underlying state, also return true if the internal state was updated from the last snapshot
 type SnapshotView[V any] interface {
 	Snapshot() (V, bool)
@@ -13,5 +19,9 @@ type SnapshotView[V any] interface {
 // An interface for an object to provide its state and update it
 type StateStore[X any] interface {
 	UpdateState(data X) error
+	ReadonlyStateStore[X]
+}
+
+type ReadonlyStateStore[X any] interface {
 	GetState() X
 }
