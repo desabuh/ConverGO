c784e0aa33061989fbf7389761b357477dfdd406
Sabuh <mastersabuh@gmail.com>
2026-07-29 00:48:14 +0200

    implemented formatted logger decorator implementation over MutexLogger

---
+++ log/formatted.go
@@ -0,0 +1,51 @@
+package log
+
+import (
+	"io"
+
+	"github.com/desabuh/convergo/config"
+)
+
+// A formattable mutex logger, provide along with each mutual exclusion system log an identifier prefix
+type FormattedMutexLogger struct {
+	logger   *MutexLogger
+	out      io.Writer
+	idPrefix string
+}
+
+func (l *FormattedMutexLogger) Log(content string, args ...any) {
+	l.logger.PrefNlog(l.idPrefix, l.out, content, args...)
+}
+
+type FormattedMutexLoggerFactory struct {
+	logger *MutexLogger
+	config.Configurable[config.LoggerConfig]
+}
+
+func NewMutexLoggerFactory(configurable config.Configurable[config.LoggerConfig]) *FormattedMutexLoggerFactory {
+	return &FormattedMutexLoggerFactory{
+		logger:       NewMutexLogger(),
+		Configurable: configurable,
+	}
+}
+
+func (f *FormattedMutexLoggerFactory) Create(id string) GlobalLogger {
+
+	loggerConfig := f.ExtractFrom(map[string]any{})
+
+	return &FormattedMutexLogger{
+		logger:   f.logger,
+		out:      loggerConfig.OutStream,
+		idPrefix: id,
+	}
+}
+
+func (f *FormattedMutexLoggerFactory) CreateFromArgs(id string, args map[string]any) GlobalLogger {
+	loggerConfig := f.ExtractFrom(args)
+
+	return &FormattedMutexLogger{
+		logger:   f.logger,
+		out:      loggerConfig.OutStream,
+		idPrefix: id,
+	}
+}
---
+++ log/global.go
@@ -0,0 +1,15 @@
+package log
+
+import "github.com/desabuh/convergo/config"
+
+// a singleton log factory whose behavior is customized based on target textual identifier
+type GlobalLoggerFactory interface {
+	Create(id string) GlobalLogger
+	CreateFromArgs(id string, args map[string]any) GlobalLogger
+	config.Configurable[config.LoggerConfig]
+}
+
+// a global logger, provide a single api to provide a system-wide service over a single underlying logger
+type GlobalLogger interface {
+	Log(content string, args ...any)
+}
