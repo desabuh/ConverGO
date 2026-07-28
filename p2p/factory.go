4d9b61dbe39e9529411fc1277fe9d12dec698d70
Sabuh <mastersabuh@gmail.com>
2026-07-29 00:47:11 +0200

    NetworkModule factory and basic TCPJsonPeerModule implementation

---
+++ p2p/factory.go
@@ -0,0 +1,69 @@
+package p2p
+
+import (
+	"github.com/desabuh/convergo/config"
+)
+
+// a generic factory to return a new NetworkModule[M, T, R], provide two methods:
+// - Create accept the module identifier T
+// - CreateFromArgs provide other that the identifier also additional arguments to override the factory default behavior
+// Should also implement a Configurable interface to extract configuration from the hashmap
+type NetworkModuleFactory[M comparable, T comparable, R comparable] interface {
+	Create(id T) NetworkModule[M, T, R]
+	CreateFromArgs(id T, args map[string]any) NetworkModule[M, T, R]
+	config.Configurable[config.NetworkModuleConfig]
+}
+
+type NetworkModule[M comparable, T comparable, R comparable] struct {
+	Transport[M, T, R]
+	MessageBroker[M, T]
+	Enveloper[R]
+}
+
+// a factory that return a new NetworkModule based on TCP transport, json based encoding and PeerMessage based communication
+type TCPJsonPeerNetworkModuleFactory struct {
+	config.Configurable[config.NetworkModuleConfig]
+}
+
+func (nm TCPJsonPeerNetworkModuleFactory) Create(id PeerHostInfo) NetworkModule[PeerMessage, PeerHostInfo, PeerMetadata] {
+
+	networkConfig := nm.ExtractFrom(map[string]any{})
+
+	return nm.initModule(id, networkConfig)
+
+}
+
+func (nm TCPJsonPeerNetworkModuleFactory) CreateFromArgs(id PeerHostInfo, args map[string]any) NetworkModule[PeerMessage, PeerHostInfo, PeerMetadata] {
+
+	networkConfig := nm.ExtractFrom(args)
+
+	return nm.initModule(id, networkConfig)
+
+}
+
+func (nm TCPJsonPeerNetworkModuleFactory) initModule(id PeerHostInfo, networkConfig config.NetworkModuleConfig) NetworkModule[PeerMessage, PeerHostInfo, PeerMetadata] {
+
+	jsonTransportCodec := NewCodec(JsonEnvelopeEncoder[PeerMetadata]{}, JsonEnvelopeDecoder[PeerMetadata]{})
+
+	var clusterTransport = NewTCPTransportWithHostExhange(id, jsonTransportCodec)
+	clusterTransport.SetCodec(jsonTransportCodec)
+
+	var jsonEnveloper Enveloper[PeerMetadata] = func(meta PeerMetadata, payload any) (Envelope[PeerMetadata], error) {
+		if payload == nil {
+			return GetEmptyEnvelope(meta), nil
+		} else {
+			return GetJsonBasedEnvelope(meta, payload)
+		}
+	}
+
+	clusterTransport.SetLocalMsgFactory(jsonEnveloper)
+
+	broker := NewPeerMessageBroker(clusterTransport, networkConfig.QueueSize)
+
+	return NetworkModule[Envelope[PeerMetadata], PeerHostInfo, PeerMetadata]{
+		Transport:     clusterTransport,
+		MessageBroker: broker,
+		Enveloper:     jsonEnveloper,
+	}
+
+}
