8285addedeb12526ce2961a4a269fa41e8ee0e95
Sabuh <mastersabuh@gmail.com>
2026-09-25 00:24:37 +0200

    simplified signature for ClusterNodeStore

--- cluster/clusters.go
+++ cluster/clusters.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"sync"
 
 	"github.com/desabuh/convergo/p2p"
 )
@@ -17,7 +16,6 @@ type Node interface {
 	GetId() p2p.PeerHostInfo
 	Init(ctx context.Context)
 	ShutDown()
-	IsActive() bool
 }
 
 type NodeKind string
@@ -28,7 +26,6 @@ const (
 )
 
 type ClusterNodeStore struct {
-	mu       sync.Mutex
 	nodes    map[p2p.PeerHostInfo]Node
 	defaults map[NodeKind]p2p.PeerHostInfo
 	factory  PeerAppNodeModuleFactory
@@ -42,53 +39,92 @@ func NewClusterNodeStore(factory PeerAppNodeModuleFactory) *ClusterNodeStore {
 	}
 }
 
-func (s *ClusterNodeStore) CreateNewClient(ctx context.Context, clusterInfo p2p.PeerHostInfo, id p2p.PeerHostInfo) error {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	client := CreateNewClusterClient(clusterInfo, id, s.factory)
-	return s.init(ctx, client)
+func GetNodeAs[T Node](id p2p.PeerHostInfo, node Node) (T, error) {
+	nodeCasted, ok := node.(T)
+	if !ok {
+		return *new(T), fmt.Errorf("id %s is associated with the wrong type of node (client/server)", id.Format())
+	}
+	return nodeCasted, nil
 }
 
-func (s *ClusterNodeStore) CreateNewServer(ctx context.Context, id p2p.PeerHostInfo) error {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	server := CreateNewClusterServer(id, s.factory)
-	return s.init(ctx, server)
-}
-
-func (s *ClusterNodeStore) exists(node Node) bool {
-	_, ok := s.nodes[node.GetId()]
-	return ok
-}
-
-func (s *ClusterNodeStore) init(ctx context.Context, node Node) error {
+func (s *ClusterNodeStore) init(ctx context.Context, node Node, kind NodeKind) error {
 	id := node.GetId()
 
 	_, ok := s.nodes[id]
 
 	if ok {
-		return fmt.Errorf("A node with id %s already exists", id)
+		return fmt.Errorf("A node with id %s already exists", id.Format())
+	}
+
+	_, ok = s.defaults[kind]
+
+	if ok {
+		return fmt.Errorf("A node with kind %s is already defined in the store", kind)
 	}
 
 	s.nodes[id] = node
+	s.defaults[kind] = node.GetId()
 
-	// go func() {
-	// 	defer s.Remove(id)
-	// 	node.Init(ctx)
-	// }()
 	go node.Init(ctx)
 
 	return nil
 }
 
-func (s *ClusterNodeStore) Remove(id p2p.PeerHostInfo) error {
-	s.mu.Lock()
+func (s *ClusterNodeStore) getNode(id p2p.PeerHostInfo) (Node, error) {
+
 	node, ok := s.nodes[id]
+
 	if !ok {
-		s.mu.Unlock()
-		return fmt.Errorf("no id is found in the node store")
+		return nil, fmt.Errorf("no id is found in the node store")
+	}
+
+	return node, nil
+}
+
+func (s *ClusterNodeStore) getKindNode(kind NodeKind) (Node, error) {
+	nodeInfo, ok := s.defaults[kind]
+
+	if !ok {
+		return nil, fmt.Errorf("no id is found in the defaults node store")
+	}
+
+	return s.getNode(nodeInfo)
+}
+
+func (s *ClusterNodeStore) CreateNewClient(ctx context.Context, clusterInfo p2p.PeerHostInfo, id p2p.PeerHostInfo) error {
+	client := CreateNewClusterClient(clusterInfo, id, s.factory)
+	return s.init(ctx, client, NodeKindClient)
+}
+
+func (s *ClusterNodeStore) CreateNewServer(ctx context.Context, id p2p.PeerHostInfo) error {
+	server := CreateNewClusterServer(id, s.factory)
+	return s.init(ctx, server, NodeKindServer)
+}
+
+func (s *ClusterNodeStore) GetClient() (*ClusterClient, error) {
+	node, err := s.getKindNode(NodeKindClient)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return GetNodeAs[*ClusterClient](node.GetId(), node)
+}
+
+func (s *ClusterNodeStore) GetServer() (*ClusterServer, error) {
+	node, err := s.getKindNode(NodeKindServer)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return GetNodeAs[*ClusterServer](node.GetId(), node)
+}
+
+func (s *ClusterNodeStore) Remove(id p2p.PeerHostInfo) error {
+	node, err := s.getNode(id)
+	if err != nil {
+		return err
 	}
 
 	delete(s.nodes, id)
@@ -97,136 +133,22 @@ func (s *ClusterNodeStore) Remove(id p2p.PeerHostInfo) error {
 			delete(s.defaults, kind)
 		}
 	}
-	s.mu.Unlock()
 
+	//no error is returned by ShutDown (intended behavior, the node will log the error themselves)
 	node.ShutDown()
 	return nil
 }
 
-func (s *ClusterNodeStore) GetDefaultClient() (*ClusterClient, error) {
-	return s.getDefaultClient()
-}
-
-func (s *ClusterNodeStore) GetDefaultServer() (*ClusterServer, error) {
-	return s.getDefaultServer()
-}
-
-func (s *ClusterNodeStore) SetDefaultClient(id p2p.PeerHostInfo) error {
-	return s.setDefault(NodeKindClient, id)
-}
-
-func (s *ClusterNodeStore) SetDefaultServer(id p2p.PeerHostInfo) error {
-	return s.setDefault(NodeKindServer, id)
-}
-
-func (s *ClusterNodeStore) setDefault(kind NodeKind, id p2p.PeerHostInfo) error {
-	s.mu.Lock()
-	node, ok := s.nodes[id]
-	s.mu.Unlock()
-
-	if !ok {
-		return fmt.Errorf("cannot set default %s: no id associated with %s in store", kind, id.Format())
-	}
-
-	switch kind {
-	case NodeKindClient:
-		if _, ok := node.(*ClusterClient); !ok {
-			return fmt.Errorf("cannot set default client: id %s is not a client", id.Format())
-		}
-	case NodeKindServer:
-		if _, ok := node.(*ClusterServer); !ok {
-			return fmt.Errorf("cannot set default server: id %s is not a server", id.Format())
-		}
-	default:
-		return fmt.Errorf("unknown node kind %q", kind)
-	}
-
-	s.mu.Lock()
-	s.defaults[kind] = id
-	s.mu.Unlock()
-	return nil
-}
-
-func (s *ClusterNodeStore) getDefaultID(kind NodeKind) (p2p.PeerHostInfo, error) {
-	s.mu.Lock()
-	id, ok := s.defaults[kind]
-	s.mu.Unlock()
-
-	if !ok {
-		return p2p.PeerHostInfo{}, fmt.Errorf("no default %s was currently set", kind)
-	}
-
-	return id, nil
-}
-
-func (s *ClusterNodeStore) getNode(id p2p.PeerHostInfo) (Node, error) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	node, ok := s.nodes[id]
-
-	if !ok {
-		return nil, fmt.Errorf("no id is found in the node store")
-	}
-
-	if !node.IsActive() {
-		return nil, fmt.Errorf("node already shut down")
-	}
-
-	return node, nil
-}
-
-func (s *ClusterNodeStore) GetClient(id p2p.PeerHostInfo) (*ClusterClient, error) {
-	node, err := s.getNode(id)
-	if err != nil {
-		return nil, err
-	}
-	return GetNodeAs[*ClusterClient](id, node)
-}
-
-func (s *ClusterNodeStore) GetServer(id p2p.PeerHostInfo) (*ClusterServer, error) {
-	node, err := s.getNode(id)
-	if err != nil {
-		return nil, err
-	}
-	return GetNodeAs[*ClusterServer](id, node)
-}
-
-func (s *ClusterNodeStore) getDefaultClient() (*ClusterClient, error) {
-	id, err := s.getDefaultID(NodeKindClient)
-	if err != nil {
-		return nil, err
-	}
-	return s.GetClient(id)
-}
-
-func (s *ClusterNodeStore) getDefaultServer() (*ClusterServer, error) {
-	id, err := s.getDefaultID(NodeKindServer)
-	if err != nil {
-		return nil, err
-	}
-	return s.GetServer(id)
-}
-
 func (s *ClusterNodeStore) Close() error {
-	s.mu.Lock()
+
 	ids := make([]p2p.PeerHostInfo, 0, len(s.nodes))
 	for id := range s.nodes {
 		ids = append(ids, id)
 	}
-	s.mu.Unlock()
 
 	var err error
 	for _, id := range ids {
 		err = errors.Join(err, s.Remove(id))
 	}
 	return err
-}
-
-func GetNodeAs[T Node](id p2p.PeerHostInfo, node Node) (T, error) {
-	nodeCasted, ok := node.(T)
-	if !ok {
-		return *new(T), fmt.Errorf("id %s is associated with the wrong type of node (client/server)", id.Format())
-	}
-	return nodeCasted, nil
 }
--- cluster/nodemodule.go
+++ cluster/nodemodule.go
@@ -3,7 +3,6 @@ package cluster
 import (
 	"context"
 	"errors"
-	"sync/atomic"
 
 	"github.com/desabuh/convergo/log"
 	"github.com/desabuh/convergo/p2p"
@@ -17,16 +16,10 @@ type AppNodeModule[M comparable, T comparable, R comparable] struct {
 	Id T
 	p2p.NetworkModule[M, T, R]
 	log.GlobalLogger
-
-	moduleShutdown *atomic.Bool
 }
 
 func (a *AppNodeModule[M, T, R]) GetId() T {
 	return a.Id
-}
-
-func (a *AppNodeModule[M, T, R]) IsActive() bool {
-	return !a.moduleShutdown.Load()
 }
 
 func (a *AppNodeModule[M, T, R]) InitModule(ctx context.Context) {
@@ -35,11 +28,6 @@ func (a *AppNodeModule[M, T, R]) InitModule(ctx context.Context) {
 }
 
 func (a *AppNodeModule[M, T, R]) ShutDownModule() error {
-	if !a.IsActive() {
-		return MODULE_ALREADY_SHUT_DOWN
-	}
-
-	a.moduleShutdown.Store(true)
 
 	a.Log("Shutting down application module...")
 	a.MessageBroker.Stop()
@@ -59,10 +47,9 @@ func (a AppNodeModuleFactory[M, T, R]) Create(info T) AppNodeModule[M, T, R] {
 	logger := a.LoggerFactory.CreateFromArgs(info.Format(), map[string]any{})
 
 	return AppNodeModule[M, T, R]{
-		Id:             info,
-		NetworkModule:  networkModule,
-		GlobalLogger:   logger,
-		moduleShutdown: new(atomic.Bool),
+		Id:            info,
+		NetworkModule: networkModule,
+		GlobalLogger:  logger,
 	}
 }
 
@@ -72,10 +59,9 @@ func (a AppNodeModuleFactory[M, T, R]) CreateFromArgs(info T, args map[string]an
 	logger := a.LoggerFactory.CreateFromArgs(info.Format(), args)
 
 	return AppNodeModule[M, T, R]{
-		Id:             info,
-		NetworkModule:  networkModule,
-		GlobalLogger:   logger,
-		moduleShutdown: new(atomic.Bool),
+		Id:            info,
+		NetworkModule: networkModule,
+		GlobalLogger:  logger,
 	}
 
 }
